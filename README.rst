5d40edc899e3f3a9bb41eabb0351d8ef324bb1ce
Grégory Salvan <apieum@gmail.com>
2013-09-29 14:51:17 +0200

    added minimal project configuration: .gitignore, setup and readme

---
+++ README.rst
@@ -0,0 +1,12 @@
+===========
+Eventize
+===========
+---------------------------------------------------------------------
+Add events to object methods and attributes
+---------------------------------------------------------------------
+
+*usage*
+ see tests::
+
+   # cd eventize
+   # nosetests --with-spec --spec-color ./
---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf8 -*-
+from setuptools import setup
+import os
+
+pkgName = 'eventize'
+setup(
+    name=pkgName,
+    version='0.1',
+    url='http://www.python.org/pypi/' + pkgName,
+    author='Grégory Salvan',
+    author_email='apieum@gmail.com',
+    license='LGPL',
+    description='Add events to object methods and attributes',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Planning",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    include_package_data=True,
+    packages=['eventize'],
+    zip_safe=True,
+)
