8364513a34fb7ec2addda37f17f9fa2517c0ab00
Grégory Salvan <apieum@gmail.com>
2013-10-03 01:11:28 +0200

    added decorators to observe methods

--- eventize/eventize.py
+++ eventize/eventize.py
@@ -1,1 +1,11 @@
 # -*- coding: utf8 -*-
+from method import Method
+
+def ObservedMethod(method):
+    return Method(method)
+
+def Observable(cls):
+    for attr, value in cls.__dict__.items():
+        if attr[0] != '_' and callable(value):
+            setattr(cls, attr, Method(value))
+    return cls
--- eventize/tests/testEventize.py
+++ eventize/tests/testEventize.py
@@ -1,5 +1,33 @@
 # -*- coding: utf8 -*-
 import unittest
+from mock import Mock
+from eventize.method import Method
+from eventize.eventize import Observable, ObservedMethod
+
 
 class EventizeTest(unittest.TestCase):
-    pass
+    def test_it_can_make_all_object_methods_observable(self):
+        @Observable
+        class Observed(object):
+            def method(self):
+                return True
+
+        observed = Observed()
+        self.assertTrue(observed.method())
+        self.assertIsInstance(Observed.method, Method)
+        observed.method.on += Mock()
+        self.assertTrue(hasattr(Observed.method, 'on'))
+        self.assertTrue(hasattr(observed.method, 'on'))
+
+    def test_it_can_make_methods_observable(self):
+        class Observed(object):
+            @ObservedMethod
+            def method(self):
+                return True
+
+        observed = Observed()
+        self.assertTrue(observed.method())
+        self.assertIsInstance(Observed.method, Method)
+        Observed.method.before += Mock()
+        self.assertTrue(hasattr(Observed.method, 'before'))
+        self.assertTrue(hasattr(observed.method, 'before'))
