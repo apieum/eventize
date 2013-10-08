00653b2c879ab31731d8d13f11ea05ed3730a434
Grégory Salvan <apieum@gmail.com>
2013-10-08 03:42:17 +0200

    define __name__, __code__, __defaults__, __doc__ to EventMethod from given func

--- eventize/method.py
+++ eventize/method.py
@@ -5,10 +5,8 @@ from .events import EventSlot, Event
 class Method(object):
     def __init__(self, func):
         self._assert_callable(func)
-        self.__name__ = getattr(func, '__name__', 'EventMethod%s' %id(func))
-        self.func_doc = getattr(func, '__doc__', '')
-        self.func_defaults = getattr(func, '__defaults__', tuple())
         self.__func__ = func
+        self._define_func_properties(func)
 
         events = self._set_events('__call__', self)
         self._bind_events(events, self)
@@ -23,6 +21,20 @@ class Method(object):
             self._bind_method(self.__name__, instance)
 
         return instance.__dict__[self.__name__]
+
+    def _define_func_properties(self, func):
+        if hasattr(func, '__name__'):
+            self.__name__ = func.__name__
+            if func.__name__ == '<lambda>':
+                self.__name__ = 'lambda%s' % id(func)
+        else:
+            self.__name__ = getattr(type(func), '__name__', type(self).__name__) + str(id(func))
+            func = func.__call__
+
+        self.__doc__ = getattr(func, '__doc__', '')
+        self.__defaults__ = getattr(func, '__defaults__', None)
+        self.__code__ = getattr(func, '__code__', None)
+
 
     def _bind_method(self, name, instance):
         events = self._set_events(name, instance)
