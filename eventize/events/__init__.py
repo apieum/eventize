2bd416e9b6514140db5b0bb152ed822a20a04d98
Grégory Salvan <apieum@gmail.com>
2013-10-09 03:06:48 +0200

    Extract Method and Attribute events creation... to dedicated Class

--- eventize/events/__init__.py
+++ eventize/events/__init__.py
@@ -2,5 +2,6 @@
 from .conditional import Conditional
 from .slot import Slot as EventSlot
 from .event import Event
+from .listener import Listener
 
-__all__ = ['Conditional', 'EventSlot', 'Event']
+__all__ = ['Conditional', 'EventSlot', 'Event', 'Listener']
