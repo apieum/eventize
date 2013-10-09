913e1201c370f8b5473602e48a558437523e8a2f
Grégory Salvan <apieum@gmail.com>
2013-10-09 03:08:32 +0200

    Added __get__ to named descriptor

--- eventize/namedDescriptor.py
+++ eventize/namedDescriptor.py
@@ -11,3 +11,12 @@ class NamedDescriptor(object):
         if self.__name__ is None:
             self.__name__ = self._find_name(type(instance))
         return self.__name__
+
+
+    def __get__(self, instance, ownerCls):
+        if instance is None: return self
+        name = self._get_name(instance)
+        return self._retrieve_from_name(name, instance)
+
+    def _retrieve_from_name(self, name, instance):
+        raise NotImplementedError("%s._retrieve_from_name must be implemented" % type(self).__name__)
