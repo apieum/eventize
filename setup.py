8b79e15bb6dd9a6889365e3e91659f2335ffd6be
Grégory Salvan <apieum@gmail.com>
2013-10-09 08:06:26 +0200

    Change version to 0.1.3 to update pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 pkgName = 'eventize'
 setup(
     name=pkgName,
-    version='0.1.2',
+    version='0.1.3',
     url='http://www.python.org/pypi/' + pkgName,
     author='Grégory Salvan',
     author_email='apieum@gmail.com',
