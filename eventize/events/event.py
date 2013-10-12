cb4a424fd3aac87b8e755acbfa1fce6e9c5b8c2f
Grégory Salvan <apieum@gmail.com>
2013-10-12 07:22:58 +0200

    remove tests on event

--- eventize/events/event.py
+++ eventize/events/event.py
@@ -25,15 +25,3 @@ class Event(object):
     def stop_propagation(self, msg=None):
         self.messages.append(msg)
         raise StopPropagation(msg)
-
-    def arg_equal(self, value):
-        return value == self.subject or value in self.args
-
-    def kwarg_equal(self, key, value):
-        return hasattr(self, key) and getattr(self, key) == value
-
-    def is_kwarg_instance_of(self, key, *expected_type):
-        return hasattr(self, key) and isinstance(getattr(self, key), expected_type)
-
-    def is_kwarg_type_equal(self, key, *expected_types):
-        return hasattr(self, key) and type(getattr(self, key)) in expected_types
