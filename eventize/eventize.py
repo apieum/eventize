41cf28ff9d2aa75e77e6ff55f747dc5e5d9e6208
Grégory Salvan <apieum@gmail.com>
2014-05-18 03:48:13 +0200

    refactor Observable: name lambda not_underscored for filter

--- eventize/eventize.py
+++ eventize/eventize.py
@@ -7,7 +7,8 @@ __all__ = ['Observable', 'Observer', 'handle']
 
 
 def Observable(cls):
-    fields = filter(lambda attr: not attr[0].startswith('_'), cls.__dict__.items())
+    not_underscored = lambda attr: not attr[0].startswith('_')
+    fields = filter(not_underscored, cls.__dict__.items())
     for attr, value in fields:
         setattr(cls, attr, Observer(value))
     return cls
