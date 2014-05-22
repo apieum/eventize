39881853ce9216e9919521544854a0ab66ddb645
Grégory Salvan <apieum@gmail.com>
2014-05-22 11:12:18 +0200

    Rename attribute to "default"

--- eventize/descriptors/handler.py
+++ eventize/descriptors/handler.py
@@ -4,10 +4,10 @@ from .. import events
 
 
 class Handler(events.Handler, Named):
-    handler_class = events.Handler
+    default = events.Handler
     def set_default(self, instance, alias):
         if self.is_set(instance, alias): return False
-        handler = self.handler_class(condition=self.condition)
+        handler = self.default(condition=self.condition)
         self.set(instance, alias, handler)
         return True
 
