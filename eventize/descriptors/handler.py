2bf2d129a96f2f8888bedf1f939fd3141c2baabf
Grégory Salvan <apieum@gmail.com>
2014-05-27 11:54:57 +0200

    remove override of set_default in descriptors handler and use Value type instead

--- eventize/descriptors/handler.py
+++ eventize/descriptors/handler.py
@@ -1,16 +1,15 @@
 # -*- coding: utf8 -*-
 from .named import Named
 from .. import events
-from .handlerValue import Value
+from .value import Value
 
 
 class Handler(events.Handler, Named):
     default = events.Handler
-    ValueType = Value
-    def set_default(self, instance, alias):
-        if self.is_set(instance, alias): return
-        handler = self.default(condition=self.condition)
-        instance.__dict__[alias] = self.ValueType(handler, instance, alias)
+    class ValueType(Value):
+        def init_value(self, value):
+            super(type(self), self).init_value(None)
+            self.set(value())
 
     def __hash__(self):
         return id(self)
