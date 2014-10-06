410e23bc6becfbfb1d72fd55e1b6b9b3f8535473
Grégory Salvan <apieum@gmail.com>
2014-10-06 14:06:18 +0200

    rename Observer and Observable decorators to lower case

--- eventize/api/__init__.py
+++ eventize/api/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf8 -*-
-from .decorators import Observable, Observer
+from .decorators import observable, observer
 from .constructors import handle, on_get, on_set, on_del, on_change, before, after, set_handler_type
 
+# kept for compatibility with previous versions
+Observer = observer
+Observable = observable
 
-__all__ = ['Observable', 'Observer', 'handle', 'on_get', 'on_set', 'on_del', 'on_change', 'before', 'after', 'set_handler_type']
+__all__ = ['Observable', 'Observer','observable', 'observer', 'handle', 'on_get', 'on_set', 'on_del', 'on_change', 'before', 'after', 'set_handler_type']
--- eventize/api/decorators.py
+++ eventize/api/decorators.py
@@ -1,15 +1,15 @@
 # -*- coding: utf8 -*-
 from ..typing import resolve_type
-__all__ = ['Observable', 'Observer']
+__all__ = ['observable', 'observer']
 
 
-def Observable(cls):
+def observable(cls):
     not_underscored = lambda attr: not attr[0].startswith('_')
     fields = filter(not_underscored, cls.__dict__.items())
     for attr, value in fields:
-        setattr(cls, attr, Observer(value))
+        setattr(cls, attr, observer(value))
     return cls
 
-def Observer(value):
+def observer(value):
     obs_type = resolve_type(value)
     return value if isinstance(value, obs_type) else obs_type(value)
