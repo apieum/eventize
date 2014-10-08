16831f90f35d8e977810d334041b42b99ee22a6e
Grégory Salvan <apieum@gmail.com>
2014-10-08 19:18:23 +0200

    Use metadata in doc conf

--- eventize/metadata.py
+++ eventize/metadata.py
@@ -2,11 +2,13 @@ import os
 __all__=['name', 'version', 'url', 'author', 'author_email', 'license', 'description', 'long_description', 'classifiers', 'packages']
 
 name = 'eventize'
-version='0.4.3'
+category = 'Software Development :: Libraries :: Python Modules'
+release = version='0.4.3'
 url='http://www.python.org/pypi/' + name
-author='Gregory Salvan'
+author='Grégory Salvan'
 author_email='apieum@gmail.com'
 license='LGPL'
+copyright = '(C)2014, %s' % author
 description='Add events to object methods and attributes'
 doc_source = os.path.join(os.path.dirname(__file__), 'doc', 'source')
 long_description=open(os.path.join(doc_source, 'readme.rst')).read()
@@ -18,6 +20,6 @@ classifiers=[
     "Intended Audience :: Developers",
     "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
     "Operating System :: OS Independent",
-    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: %s" % category,
 ]
 packages=['eventize', 'eventize.api', 'eventize.events', 'eventize.descriptors', 'eventize.method', 'eventize.attribute', 'eventize.typing', 'eventize.modifiers']
