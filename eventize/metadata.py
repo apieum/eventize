526cb58ab9977360dfb70fcaf87107ae5395a454
Grégory Salvan <apieum@gmail.com>
2014-10-08 19:31:09 +0200

    Fix encoding in metadata

--- eventize/metadata.py
+++ eventize/metadata.py
@@ -1,18 +1,19 @@
+# -*- coding: utf8 -*-
 import os
 __all__=['name', 'version', 'url', 'author', 'author_email', 'license', 'description', 'long_description', 'classifiers', 'packages']
 
 name = 'eventize'
 category = 'Software Development :: Libraries :: Python Modules'
-release = version='0.4.3'
-url='http://www.python.org/pypi/' + name
-author='Grégory Salvan'
-author_email='apieum@gmail.com'
-license='LGPL'
-copyright = '(C)2014, %s' % author
-description='Add events to object methods and attributes'
+release = version = '0.4.3'
+url = 'http://www.python.org/pypi/' + name
+author = u'Grégory Salvan'
+author_email ='apieum@gmail.com'
+license ='LGPL'
+copyright = u'(C)2014, %s' % author
+description ='Add events to object methods and attributes'
 doc_source = os.path.join(os.path.dirname(__file__), 'doc', 'source')
-long_description=open(os.path.join(doc_source, 'readme.rst')).read()
-classifiers=[
+long_description =open(os.path.join(doc_source, 'readme.rst')).read()
+classifiers = [
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
     "Development Status :: 4 - Beta",
@@ -22,4 +23,4 @@ classifiers=[
     "Operating System :: OS Independent",
     "Topic :: %s" % category,
 ]
-packages=['eventize', 'eventize.api', 'eventize.events', 'eventize.descriptors', 'eventize.method', 'eventize.attribute', 'eventize.typing', 'eventize.modifiers']
+packages = ['eventize', 'eventize.api', 'eventize.events', 'eventize.descriptors', 'eventize.method', 'eventize.attribute', 'eventize.typing', 'eventize.modifiers']
