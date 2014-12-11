fd6e61483b3a71ee390c92f5d9763abf7d935069
Grégory Salvan <apieum@gmail.com>
2014-12-11 03:38:14 +0100

    Move __set__, __get__, __delete__, get, set, delete implementation to AbstractDescriptor

--- eventize/descriptors/named.py
+++ eventize/descriptors/named.py
@@ -46,27 +46,7 @@ class Named(AbstractDescriptor, Modifiable):
         default = getattr(self, 'default', None)
         instance.__dict__[alias] = self.ValueType(instance, alias, default)
 
-    def __get__(self, instance, ownerCls=None):
-        if instance is None: return self
-        return self.get(instance)
-
-    def __set__(self, instance, value):
-        if instance is None: return self
-        self.set(instance, value)
-
-    def __delete__(self, instance):
-        if instance is None: return self
-        self.delete(instance)
-
     def is_set(self, instance, alias):
         return alias in instance.__dict__
 
-    def get(self, instance):
-        return self.get_value(instance).get()
-
-    def set(self, instance, value):
-        self.get_value(instance).set(value)
-
-    def delete(self, instance):
-        self.get_value(instance).delete()
 
